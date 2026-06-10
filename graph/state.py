bcd10b0cf4a02624ce9f1d3ed6f622335670b647
Kleyton Ferreira <kleytonferreira@alu.ufc.br>
2026-06-10 18:30:00 -0300

    feat: estado compartilhado do grafo

---
+++ graph/state.py
@@ -0,0 +1,11 @@
+"""Estado compartilhado entre os nós do grafo LangGraph."""
+from typing import TypedDict
+
+class EstadoQuestao(TypedDict):
+    tema: str
+    tentativas: int
+    questao: dict
+    quality_passed: bool
+    motivo_rejeicao: str
+    habilidade_bncc: str
+    descricao_bncc: str
